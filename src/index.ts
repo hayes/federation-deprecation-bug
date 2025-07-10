4d2bc8609ddf779728e2be66857abfcd4508819f
Michael Hayes <michael.hayes@mystenlabs.com>
2025-07-10 10:00:17 -0700

    use graphql 17 to support async iteration

--- src/index.ts
+++ src/index.ts
@@ -17,6 +17,48 @@ builder.queryType({
             hasNextPage: false,
             hasPreviousPage: false,
           },
+          edges: (function* () {
+            for (let i = 1; i <= 10; i++) {
+              yield {
+                cursor: i.toString(),
+                node: i,
+              }
+            }
+          })(),
+        };
+      },
+    }),
+    promiseNumbers: t.connection({
+      type: 'Int',
+      resolve: () => {
+        return {
+          pageInfo: {
+            startCursor: '1',
+            endCursor: '10',
+            hasNextPage: false,
+            hasPreviousPage: false,
+          },
+          edges: (function* () {
+            for (let i = 1; i <= 10; i++) {
+              yield Promise.resolve({
+                cursor: i.toString(),
+                node: i,
+              })
+            }
+          })(),
+        };
+      },
+    }),
+    asyncNumbers: t.connection({
+      type: 'Int',
+      resolve: () => {
+        return {
+          pageInfo: {
+            startCursor: '1',
+            endCursor: '10',
+            hasNextPage: false,
+            hasPreviousPage: false,
+          },
           edges: (async function* () {
             for (let i = 1; i <= 10; i++) {
               yield {
@@ -47,10 +89,30 @@ builder.queryType({
             cursor
           }
         }
+        promiseNumbers {
+          pageInfo {
+            startCursor
+            endCursor
+          }
+          edges {
+            node
+            cursor
+          }
+        }
+        asyncNumbers {
+          pageInfo {
+            startCursor
+            endCursor
+          }
+          edges {
+            node
+            cursor
+          }
+        }
       }
     `,
   ),
   contextValue: {},
-})).then(console.log).catch(console.error)
+})).then(r => console.dir(r, { depth: null })).catch(console.error)
 
 
